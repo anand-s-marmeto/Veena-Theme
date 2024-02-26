67f431f8c53f2bd1f4518914ecd8f14e45c759f8
Anand <as504919@gmail.com>
2024-02-26 12:42:27 +0530

    Updated the counter section

--- assets/my-splide.js
+++ assets/my-splide.js
@@ -2,13 +2,13 @@ class MySplide extends HTMLElement {
   constructor() {
     super();
     this.element = this.querySelector(".splide");
-    this.options = JSON.parse(this.querySelector("script").textContent);
+    this.options = JSON.parse(this.dataset.sliderOptions);
     this.initializeSlider();
     console.log(this.options);
   }
 
   initializeSlider() {
-    var splide = new Splide(this.element,this.options);
+    var splide = new Splide(this.element, this.options);
     splide.mount();
   }
 }
